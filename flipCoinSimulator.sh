1ceab2d556cadc0a4e89602ae19067728977ec12
shinekumar23 <shinekumar23@gmail.com>
2020-08-04 20:38:43 +0530

    Added loop to show no. of times won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -x
-flip=$((RANDOM%2))
+headsWon=0
+tailsWon=0
 
-case $flip in
-         0) echo "Heads is the winner"
-            ;;
-         1) echo "Tails is the winner"
-            ;;
-esac
+for (( i=1; i<=10; i++ ))
+do
+			flip=$((RANDOM%2))
+			case $flip in
+         				0) echo "Heads is the winner"
+            				((headsWon++))
+            				;;
+         				1) echo "Tails is the winner"
+            				((tailsWon++))
+            				;;
+			esac
+done
+echo "Heads Won $headsWon times"
+echo "Tails Won $tailsWon times"
+ 
+
+
+
+
 
