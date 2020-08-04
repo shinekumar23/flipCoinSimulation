00c5d23b72e5083c252ee1542df12edb0fa48611
shinekumar23 <shinekumar23@gmail.com>
2020-08-04 20:24:21 +0530

    Heads or Tails Won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,10 @@
-echo "Welcome to flip coin simulator"
+#!/bin/bash -x
+flip=$((RANDOM%2))
+
+case $flip in
+         0) echo "Heads is the winner"
+            ;;
+         1) echo "Tails is the winner"
+            ;;
+esac
+
