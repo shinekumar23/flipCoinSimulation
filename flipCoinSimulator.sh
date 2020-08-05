1e658036f3fa9f331ec784931b083639886166b8
shinekumar23 <shinekumar23@gmail.com>
2020-08-05 11:23:39 +0530

    Added winning strategy if game is tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,15 +4,18 @@ tailsWon=0
 
 for (( i=1; i<=22; i++ ))
 do
-			flip=$((RANDOM%2))
-			case $flip in
-         				0) echo "Heads is the winner"
-            				((headsWon++))
-            				;;
-         				1) echo "Tails is the winner"
-            				((tailsWon++))
-            				;;
-			esac
+			function flipping() {
+								flip=$((RANDOM%2))
+								case $flip in
+         									0) echo "Heads"
+            									((headsWon++))
+            								 	;;
+         									1) echo "Tails"
+            									((tailsWon++))
+            									;;
+								esac
+			}
+			flipping
 done
 if [ $headsWon -gt $tailsWon ]
 then
@@ -24,10 +27,19 @@ else
                echo "Its a tie. Both have $headsWon points"
 fi 
 
-
- 
-
-
-
+if [ $headsWon == $tailsWon ]
+then
+			 echo "Flipping again to choose the winner who have acheieved a difference of minimum 2 points"
+       	 while [[ $headsWon -lt $(($tailsWon+2)) && $tailsWon -lt $(($headsWon+2)) ]]
+        	 do
+							flipping
+			 done
+          if [ $headsWon -gt $tailsWon ]
+			 then
+            				echo "Heads won $headsWon times and TailsWon $tailsWon times. Heads won by a difference of 2 points"
+			 else
+            				echo "Tails won $tailsWon times and Heads Won $headsWon times. Tails won by a difference of 2 points"
+			 fi
+fi
 
 
