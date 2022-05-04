81712063c6833927cbdfbc255dcde442d061c6b7
SHINE KUMAR K P <47713845+shinekumar23@users.noreply.github.com>
2022-05-04 12:37:42 +0530

    Update flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,7 +4,7 @@ flip=$((RANDOM%2))
 case $flip in
          0) echo "Heads is the winner"
             ;;
-         1) echo "Tails is the winner"
+         1) echo "Tails is the winner-yes"
             ;;
 esac
 
